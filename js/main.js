9ffed05044647012e89d7975cbd5699a7978ccc9
Abigail Friedman <abigailfriedman@Abigails-MacBook-Air.local>
2021-03-13 22:06:03 -0500

    added backstretch jquery

---
+++ js/main.js
@@ -0,0 +1,8 @@
+///Adding https://www.jquery-backstretch.com/
+
+$(".hero").backstretch([
+  "images/Home1.jpg",
+  "images/home/Home2.jpg",
+  "images/home/Home3.jpg",
+  "images/home/Home4.jpg"
+], {duration: 3000, transition: 'fade', transitionDuration: 2000, animateFirst:false});
